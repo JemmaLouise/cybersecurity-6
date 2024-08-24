6093abb5a59303ab4225735704d7a9da2303c1a7
hannahantigua <168355533+hannahantigua@users.noreply.github.com>
2024-08-24 10:20:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:p0psicl3@localhost/movies'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/movies'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TMDB_API_KEY = os.environ.get('TMDB_API_KEY') or '5609df2250e4c3355f12c198f1353ca6'
